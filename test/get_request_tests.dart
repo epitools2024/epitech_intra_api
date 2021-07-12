9cc412247278620769774c0eac3edba51ee081c9
nemesisx1 <nemesisx1@HP-Laptop-15-15-da1026nk-e0e6a4d0.localdomain>
2021-07-12 13:52:14 +0000

    [update] tests for either

--- test/get_request_tests.dart
+++ test/get_request_tests.dart
@@ -1,7 +1,6 @@
 import 'package:epitech_intra_api/epitech_intra_api.dart';
 import 'package:epitech_intra_api/helpers/endpoints.dart';
 import 'package:epitech_intra_api/helpers/get_requests.dart';
-import 'package:epitech_intra_api/helpers/logger.dart';
 import 'package:test/test.dart';
 
 final EPITECH_MAIL = 'junior.medehou@epitech.eu';
@@ -12,46 +11,51 @@ final AUTOLOGIN = 'auth-57a0d4528e3117b98cffc0e2a0c996ac4f14808c';
 void main() async {
   final intra = EpitechAPI(
     mail: EPITECH_MAIL,
-    enableLogs: true,
+    //enableLogs: true,
     autologin: AUTOLOGIN_URL,
   );
 
+  // With the Either used in the get method,
+  // the Right refers to the non-error part
+  // and the Left to the error one
   group('Test on GetRequest extension on EpitechAPI', () {
     test('Test on all', () async {
-      final res = intra.get(EndPoint.all);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.get(EndPoint.all)).isRight();
+      expect(res, true);
     });
     test('Test on alert', () async {
-      final res = await intra.get(EndPoint.alert);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.get(EndPoint.alert)).isRight();
+      expect(res, true);
     });
-    test('Test on message', () {
-      final res = intra.get(EndPoint.message);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+    test('Test on message', () async {
+      final res = ((await intra.get(EndPoint.message)).isRight());
+      expect(res, true);
     });
     test('Test on marks', () async {
-      final res = intra.get(EndPoint.marks);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.get(EndPoint.marks)).isRight();
+      expect(res, true);
     });
 
     test('Test on missedRdv', () async {
-      final res = await intra.get(EndPoint.missedRdv);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.get(EndPoint.missedRdv)).isRight();
+      expect(res, true);
     });
 
     test('Test on nextRdv', () async {
-      final res = await intra.get(EndPoint.nextRdv);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.get(EndPoint.nextRdv)).isRight();
+      expect(res, true);
     });
 
     test('Test on raw queries', () async {
-      final res = await intra.getRaw(EndPoint.all);
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.getRaw(EndPoint.all)).isRight();
+      expect(res, true);
     });
+
     test('Test on Date queries', () async {
-      final res = await intra.getRawDate(
-          start: DateTime(2021, 01, 01), end: DateTime(2021, 01, 20));
-      expect(res, isNot(EpitechErrorType.EMPTY_CALL));
+      final res = (await intra.getRawDate(
+              start: DateTime(2021, 01, 01), end: DateTime(2021, 01, 20)))
+          .isRight();
+      expect(res, true);
     });
   });
 }
