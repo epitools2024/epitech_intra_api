4f3e3ebe8c6f6b794753eff2931e94159ec43347
nemesisx1 <nemesisx1@HP-Laptop-15-15-da1026nk-e0e6a4d0.localdomain>
2021-07-12 11:17:37 +0000

    [update] students classes

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -1,16 +1,20 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class Student extends Equatable {
-  final String? gpa;
+  final double? gpa;
   final String? year;
-  final String? credit;
+  final int? credits;
+  final String? autologin;
   final String? mail;
   final String? cycle;
   final String? pictureUrl;
   final bool? isAdmin;
 
   Student({
-    this.credit,
+    this.autologin,
+    this.credits,
     this.gpa,
     this.mail,
     this.year,
@@ -18,6 +22,34 @@ class Student extends Equatable {
     this.isAdmin,
     this.pictureUrl,
   });
+
+  factory Student.empty() {
+    return Student(
+      credits: 0,
+      year: '',
+      mail: '',
+      pictureUrl: '',
+      isAdmin: false,
+      cycle: '',
+      autologin: '',
+      gpa: 0.00,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'credit': credits,
+      'year': year,
+      'mail': mail,
+      'pictureUrl': '',
+      'isAdmin': false,
+      'cycle': '',
+      'gpa': 0.00,
+      'autologin': autologin,
+    };
+  }
+
+  String toJson() => jsonEncode(toMap());
 
   @override
   List<Object> get props => [gpa!, year!, mail!, year!];
